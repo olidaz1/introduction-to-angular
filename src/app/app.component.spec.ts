28c5aae76c9a8b401e7086168c23dc1b4963f34d
Olivier DAZIN <olivier.dazin@gmail.com>
2023-10-20 18:02:51 +0200

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fairhouse app is running!');
+    expect(compiled.querySelector('header')?.textContent).toContain('Fairhouse');
   });
 });
